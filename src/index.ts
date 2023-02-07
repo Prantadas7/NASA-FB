fa0042f85705d472dc92b613ac62b6e7bcc06d52
Prantadas7 <pranta_das@coredevs.ltd>
2023-02-07 18:38:54 +0600

    done

--- src/index.ts
+++ src/index.ts
@@ -1,57 +1,56 @@
 const puppeteer = require('puppeteer');
 import { executablePath } from "puppeteer";
+require('dotenv').config();
+// const user: string = process.env.USERNAME as string;
+// const pass: string = process.env.PASS as string;
 
 (async () => {
+    const sleep = async () => await new Promise((r) => setTimeout(r, 100));
     const browser = await puppeteer.launch({
         slowMo: 100,
-        headless: false, executablePath: executablePath(), args: ['--disable-infobars', '--start-maximized', '--disable-blink-features=PasswordGeneration,PasswordManager'], ignoreDefaultArgs: ['--enable-automation']
+        ignoreHTTPSErrors: true,
+        headless: false, executablePath: executablePath(), args: ['--disable-infobars', '--disable-notifications', '--start-maximized', '--disable-blink-features=PasswordGeneration,PasswordManager', '--disable-web-notification-custom-layouts', "--disable-popup-blocking", "--allow-popups-during-page-unload", "--no-sandbox", "--disable-popup-blocking", "--disable-permissions-api", '--enable-automation'], ignoreDefaultArgs: ['--disable-save-password-bubble', '--enable-automation']
     });
+    const context = browser.defaultBrowserContext();
+    await context.overridePermissions('https://www.facebook.com', ["geolocation", "midi", "notifications", "camera", "microphone", "background-sync", "ambient-light-sensor", "accelerometer", "gyroscope", "magnetometer", "accessibility-events", "clipboard-read", "clipboard-write", "payment-handler", "idle-detection", "midi-sysex"
+    ]);
     const page = await browser.newPage();
-
-    page.on('dialog', async (dialog: any) => {
-        await dialog.dismiss();
-    });
-
-
+    await page.setRequestInterception(false);
+    // await page.setJavaScriptEnabled(false);
     await page.setViewport({ width: 1920, height: 1080 });
 
-    // Login to Facebook
-    await page.goto('https://facebook.com/');
-    await page.type('#email', 'pranta_das@coredevs.ltd');
-    await page.type('#pass', 'Abir@141219');
-    await new Promise((r) => setTimeout(r, 100));
-    await page.click("button[type='submit']");
-    await page.waitForNavigation();
-    await new Promise((r) => setTimeout(r, 100));
-
-    await page.goto('https://facebook.com/');
-    await new Promise((r) => setTimeout(r, 100));
-    await page.on('dialog', async (dialog: any) => {
+    page.on('dialog', async (dialog: any) => {
+        console.log(dialog.message());
         await dialog.dismiss();
     });
-    await new Promise((r) => setTimeout(r, 100));
-    const [button] = await page.$x("//button[contains(., 'Photo/video')]");
-    if (button) {
-        await button.click();
-    }
 
-    // if (element) { console.log(true) }    // Post a picture every 24 hours
-    // setInterval(async () => {
-    //     // Go to your profile
-    //     // await page.goto('https://www.facebook.com/profile.php?id=100089928259018');
+    // Login to Facebook
+    await page.goto('https://facebook.com/', { waitUntill: 'documentloaded' });
 
-    //     // Click on the "Create Post" button
-    //     await page.click('[data-testid="status-attachment-add-photo-video-button"]');
+    await page.type('#email', 'prantaabir856@gmail.com', { delay: 200 });
+    await page.waitForTimeout(600);
 
-    //     // Select the picture to be posted
-    //     const fileInput = await page.$('input[type="file"]');
-    //     await fileInput.uploadFile('<YOUR_PICTURE_FILE_NAME>.jpg');
+    await page.type('#pass', 'Pranta@141219', { delay: 400 });
 
-    //     // Add a caption to the picture
-    //     await page.type('[data-testid="status-attachment-mentions-input"]', '<YOUR_CAPTION>');
+    await sleep();
 
-    //     // Post the picture
-    //     await page.click('[data-testid="react-composer-post-button"]');
-    //     await page.waitFor(2000);
-    // }, 24 * 60 * 60 * 1000);
+    await page.click("button[type='submit']");
+
+    await page.waitForNavigation();
+
+
+    // await page.goto('https://facebook.com/');
+    await sleep();
+
+    await page.on('dialog', async (dialog: any) => {
+        console.log(dialog.message());
+        await dialog.dismiss();
+    });
+    await sleep();
+
+    // await page.click('#checkpointSubmitButton');
+
+    await page.waitForSelector("What's on your mind, Rcs?")
+    await sleep();
+
 })();
